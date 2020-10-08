1d13f8835442762d56813e99aba074417fa6f5cf
roberthai <63133157+roberthai@users.noreply.github.com>
2020-10-08 10:09:28 +0700

    Add files via upload

---
+++ example.py
@@ -0,0 +1,8 @@
+nam = input("Enter your name: ")
+print("Hello " + nam + "!")
+
+birth_year = input("Birth year: ")
+print(type(birth_year))
+age = 2020 - int(birth_year)
+print(type(age))
+print (age)
